105adb825d2aa48eb7a4710c34fbd5f05222319d
marina <marinaaltesmagret@gmail.com>
2021-06-02 13:28:36 +0200

    done with props

--- src/App.js
+++ src/App.js
@@ -1,39 +1,36 @@
 import './App.css';
 import Box from './components/Box';
 
-const box1 = {'source': "./images/icon1.png", 'alt': 'icon1', 'title': 'Declarative', 'text': (<p> React makes it <br /> painless to create <br /> interactive UIs.</p>)};
-const box2 = {'source': "./images/icon2.png", 'alt': 'icon2', 'title': 'Components', 'text': (<p> Build encapsulated <br /> components that <br /> manage their state.</p>)};
-const box3 = {'source': "./images/icon3.png", 'alt': 'icon3', 'title': 'Single-Way', 'text': (<p> A set of immutable <br /> values are passed to <br /> the components.</p>)};
-const box4 = {'source': "./images/icon4.png", 'alt': 'icon4', 'title': 'JSX', 'text': (<p> Statically-typed, <br /> designed to run on <br /> modern browsers.</p>)};
-
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src= "./images/react-logo.svg" alt="react-logo" className="react-logo" />
+        <img src="./images/react-logo.svg" alt="react-logo" className="react-logo" />
         <div class="App-header-container">
-
           <div className="App-header-menu">
-            <img src="./images/ironhack-logo.svg" alt="logo"/>
+            <img src="./images/ironhack-logo.svg" alt="logo" />
             <img src="./images/menu-top.svg" alt="menu" />
           </div>
 
           <div className="App-header-content">
-            <h1>Say hello to <br /> ReactJS </h1>
-            <p>You will learn how to use <br /> the most popular frontend library, <br /> and become a super Ninja developer.</p>
-            <button >Awesome!</button>
+            <h1>
+              Say hello to <br /> ReactJS{' '}
+            </h1>
+            <p>
+              You will learn how to use <br /> the most popular frontend library, <br /> and become a super Ninja
+              developer.
+            </p>
+            <button>Awesome!</button>
           </div>
-
         </div>
       </header>
 
       <main className="App-main">
         <div className="App-main-container">
-          <Box data="box" />
-          <Box data="box" />
-          <Box data="box" />
-          <Box data="box" />
+          <Box box="1" />
+          <Box box="2" />
+          <Box box="3" />
+          <Box box="4" />
         </div>
       </main>
     </div>
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,12 +1,73 @@
-function Box (data) {
-    const {source, alt, title, text} = data;
-    return (
-        <article  className="App-main-box">
-            <img src={source} alt={alt} />
-            <h2>{title}</h2>
-            {text}
-        </article>
-    )
+const box1 = {
+  source: './images/icon1.png',
+  alt: 'icon1',
+  title: 'Declarative',
+  text: (
+    <p>
+      {' '}
+      React makes it <br /> painless to create <br /> interactive UIs.
+    </p>
+  ),
+};
+const box2 = {
+  source: './images/icon2.png',
+  alt: 'icon2',
+  title: 'Components',
+  text: (
+    <p>
+      {' '}
+      Build encapsulated <br /> components that <br /> manage their state.
+    </p>
+  ),
+};
+const box3 = {
+  source: './images/icon3.png',
+  alt: 'icon3',
+  title: 'Single-Way',
+  text: (
+    <p>
+      {' '}
+      A set of immutable <br /> values are passed to <br /> the components.
+    </p>
+  ),
+};
+const box4 = {
+  source: './images/icon4.png',
+  alt: 'icon4',
+  title: 'JSX',
+  text: (
+    <p>
+      {' '}
+      Statically-typed, <br /> designed to run on <br /> modern browsers.
+    </p>
+  ),
+};
+
+function Box(props) {
+  let box;
+  // eslint-disable-next-line default-case
+  switch (props.box) {
+    case '1':
+      box = box1;
+      break;
+    case '2':
+      box = box2;
+      break;
+    case '3':
+      box = box3;
+      break;
+    case '4':
+      box = box4;
+      break;
+  }
+  const { source, alt, title, text } = box;
+  return (
+    <article className="App-main-box">
+      <img src={source} alt={alt} />
+      <h2>{title}</h2>
+      {text}
+    </article>
+  );
 }
 
 export default Box;
